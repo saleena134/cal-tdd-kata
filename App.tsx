bfd0814b5f4275866a0cd2a5179e4bb51649e8e2
Saleena Khan <saleena@Saleenas-MacBook-Air.local>
2024-06-22 14:28:24 +0530

    added sum test cases, comma seprated,clear text input

--- App.tsx
+++ App.tsx
@@ -2,18 +2,35 @@ import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 
 const App = () => {
-  const [inputFieldValue, setInputFieldValue] = useState<number>(0)
+  const [inputFieldValue, setInputFieldValue] = useState<string>("")
+  const [result, setResult] = useState<number>(0)
+
+  const handleAddANumber = (num: string) => {
+    setInputFieldValue(num)
+    let newArr: [] = []
+    num.split(',').forEach(val => {
+      if (!isNaN(parseInt(val))) {
+        newArr.push(parseInt(val))
+      }
+    });
+    console.log("result==>", newArr);
+
+    const sum = newArr?.reduce((partialSum: number, a: number) => partialSum + a, 0);
+    console.log("sums==>", sum);
+    setResult(sum)
+  }
 
   const handleClearText = () => {
-    setInputFieldValue(0)
+    setInputFieldValue("")
+    setResult(0)
   }
 
 
   return (
     <View style={styles.container}>
-      <TextInput testID='ti-to-take-number-input' value={inputFieldValue} placeholder='Enter a first number to add' onChangeText={(num: any) => setInputFieldValue(num)} style={styles.textInputField} />
+      <TextInput testID='ti-to-take-number-input' value={inputFieldValue} placeholder='Enter a first number to add' onChangeText={(num: any) => handleAddANumber(num)} style={styles.textInputField} />
       <View style={styles.textAndButtonContainer}>
-        <Text style={{ paddingHorizontal: 10 }} testID='t-display-text'>{inputFieldValue}</Text>
+        <Text style={{ paddingHorizontal: 10 }} testID='t-display-text'>{result}</Text>
         <Button title="Clear Text" testID='b-clear-text' onPress={() => handleClearText()} />
       </View>
     </View>
--- e2e/app.test.js
+++ e2e/app.test.js
@@ -1,3 +1,5 @@
+const { log } = require('detox');
+
 describe('Example', () => {
     beforeAll(async () => {
         await device.launchApp()
@@ -17,6 +19,22 @@ describe('Example', () => {
     })
     it('should tap', async () => {
         await element(by?.id('b-clear-text'))?.tap()
+        await expect(element(by?.id('t-display-text')))?.toHaveText('0')
+        await expect(element(by?.id('ti-to-take-number-input')))?.toHaveText('')
+    })
+    it('should display 1 if value is 1', async () => {
+        await element(by?.id('ti-to-take-number-input'))?.typeText('1')
+    })
+    it('should comma seprated', async () => {
+        await element(by?.id('b-clear-text'))?.tap()
+        await element(by?.id('ti-to-take-number-input'))?.typeText('2,3,4')
+        await expect(element(by?.id('ti-to-take-number-input')))?.toHaveText('2,3,4')
+    })
+
+    it('should display sum of the numbers', async () => {
+        await element(by?.id('b-clear-text'))?.tap()
+        await element(by?.id('ti-to-take-number-input'))?.typeText('1,5')
+        await expect(element(by?.id('t-display-text')))?.toHaveText('6')
     })
 
 })
